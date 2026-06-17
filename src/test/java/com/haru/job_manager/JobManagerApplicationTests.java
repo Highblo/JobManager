4fcca2377e77ecbeae44e617f750afbc1d01ba58
Highblo <nyaa.-525@outlook.jp>
2026-06-17 19:56:39 +0900

    initial Spring boot project setup

---
+++ src/test/java/com/haru/job_manager/JobManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru.job_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
